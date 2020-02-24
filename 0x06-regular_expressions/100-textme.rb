5690125c7f3c0bfec0deed2479bc647691d0c89a
amine784 <1066@holbertonschool.com>
2020-02-24 19:58:25 +0100

    holberto

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,1 +1,5 @@
-gf
+#!/usr/bin/env ruby
+print ARGV[0].scan(/\[from:(.*?)\]/).join + "," +
+      print ARGV[0].scan(/\[to:(.*?)\]/).join + "," +
+            print ARGV[0].scan(/\[flags:(.*?)\]/).join
+      
