03a3fabb24749a1b78dbc4ffd8bf9cc6ee993f81
amine784 <1066@holbertonschool.com>
2020-02-24 20:55:03 +0100

    ghklrt

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-from = print ARGV[0].scan(/\[from:(.*?)\]/).join
-to = print ARGV[0].scan(/\[to:(.*?)\]/).join
-flags = print ARGV[0].scan(/\[flags:(.*?)\]/).join
-dlm = ","
-print from + delm + to + delm + flags
+print ARGV[0].scan(/\[from:(.*?)\]/),
+      print ARGV[0].scan(/\[to:(.*?)\]/),
+            print ARGV[0].scan(/\[flags:(.*?)\]/).join
+print(",")
+
