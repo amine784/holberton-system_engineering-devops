eb0df67ebbb4b570a33c750f45d71ff405e11413
amine784 <1066@holbertonschool.com>
2020-02-24 20:57:41 +0100

    hlor

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 print ARGV[0].scan(/\[from:(.*?)\]/),
-      print ARGV[0].scan(/\[to:(.*?)\]/),
-            print ARGV[0].scan(/\[flags:(.*?)\]/).join
+      ARGV[0].scan(/\[to:(.*?)\]/),
+      ARGV[0].scan(/\[flags:(.*?)\]/).join
 print(",")
 
