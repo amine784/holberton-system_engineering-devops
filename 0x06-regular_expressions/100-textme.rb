be9d4740c6fea5323201f58c39a9690f2a597c2f
amine784 <1066@holbertonschool.com>
2020-02-24 20:31:30 +0100

    holberton

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-print ARGV[0].scan(/\[from:(.*?)\]/).join + ","+
-      print ARGV[0].scan(/\[to:(.*?)\]/).join+","+
-            print ARGV[0].scan(/\[flags:(.*?)\]/).join
+print ARGV[0].scan(/\[from:(.*?)\]/).join + "," +
+      print ARGV[0].scan(/\[to:(.*?)\]/).join +
+            "," + print ARGV[0].scan(/\[flags:(.*?)\]/).join
