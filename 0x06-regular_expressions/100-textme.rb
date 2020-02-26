325eeb90acb22092b2ffbad6d5e3d509a6744bc8
amine784 <1066@holbertonschool.com>
2020-02-26 16:09:37 +0100

    holberto

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
-reg = (.*?\)
-delim = ","
-puts ARGV[0].scan(/\[from:reg] \[to:reg] \[flags:reg]/).join(delim)
+reg = "(.*?)\"
+puts ARGV[0].scan(/\[from:reg] \[to:reg] \[flags:reg]/).join(",")
 
 
