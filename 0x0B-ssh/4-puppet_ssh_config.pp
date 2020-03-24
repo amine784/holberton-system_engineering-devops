6c5a737c8bf68a8590f51a87a61e8744dc42e204
amine784 <1066@holbertonschool.com>
2020-03-24 22:18:55 +0100

    holberto

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,6 @@
 # ssh confifuration
 exec { 'echo':
-command  => 'echo "IdentityFile ~/.ssh/holberton " >>  /etc/ssh/ssh_config',
-path     => '/bin/',
-provider => 'shell',
-}
-exec { 'echo':
-command  =>'echo "PasswordAuthentication no"  >>  /etc/ssh/ssh_config',
-path     => '/bin/',
+command  =>'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/holberton"  >>  /etc/ssh/ssh_config',
+path     => '/bin',
 provider => 'shell',
 }
