24ac32fa6bad5be84dcd7ac068a7a9f1ff2ee725
amine784 <1066@holbertonschool.com>
2020-03-24 19:24:14 +0100

    holberto

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,6 +1,4 @@
 # ssh confifuration
-exec { 'echo -e "Id_File ~/ssh/holberton\n" >> /etc/ssh/ssh_config':
-provider => shell,
-path     => '/etc/ssh/ssh_config',
-command  => 'echo -e "Pass_Auth no\n" >> /etc/ssh/ssh_config',
+exec { 'echo -e "Pass_dAuth no\n Id_File ~/.ssh/holberton" >> /etc/ssh/ssh_config':
+path => '/bin/',
 }
