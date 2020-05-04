24362e2ccf10fe4c7165256f47cf4f8241baa176
amine784 <1066@holbertonschool.com>
2020-05-04 21:46:14 +0100

    holberto

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,19 +8,19 @@ if __name__ == "__main__":
     q = req.get("{}users".format(
         "https://jsonplaceholder.typicode.com/"))
     q = q.json()
-    with open(title, "w") as json_f:
-        d = []
-        for data in q:
-            Id = data.get('id')
-            query = req.get("{}todos".format(
-                "https://jsonplaceholder.typicode.com/"),
-                            params={"userId": Id})
-            query = query.json()
-            n = data.get("username")
-            for r in query:
-                z = {}
-                z['task'] = r.get('title')
-                z['completed'] = r.get('completed')
-                z['username'] = n
-                d.append(z)
+    d = []
+    for data in q:
+        Id = data.get('id')
+        query = req.get("{}todos".format(
+            "https://jsonplaceholder.typicode.com/"),
+                        params={"userId": Id})
+        query = query.json()
+        n = data.get("username")
+        for r in query:
+            z = {}
+            z['task'] = r.get('title')
+            z['completed'] = r.get('completed')
+            z['username'] = n
+            d.append(z)
+        with open(title, "w") as json_f:
             json.dump({Id: d}, json_f)
