f4f8759941e6ec0d4d15667b3e9c2440199260a5
amine784 <1066@holbertonschool.com>
2020-05-04 21:12:32 +0100

    holberto

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""python script"""
+from sys import argv
+import json
+import requests as req
+if __name__ == "__main__":
+    title = "todo_all_employees.json"
+    q = req.get("{}users".format(
+        "https://jsonplaceholder.typicode.com/"))
+    q = q.json()
+    for data in q:
+        d = []
+        Id = data.get('id')
+    query = req.get("{}todos".format(
+        "https://jsonplaceholder.typicode.com/"), params={"userId": Id})
+    query = query.json()
+    n = data.get("username")
+    for r in query:
+        z = {}
+        z['task'] = r.get('title')
+        z['completed'] = r.get('completed')
+        z['username'] = n
+        d.append(z)
+    with open(title, "w", newline="") as json_f:
+        json.dump({Id: d}, json_f)
