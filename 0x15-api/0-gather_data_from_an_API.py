7686e067158351ed47270748e0bd5302006ad7f8
amine784 <1066@holbertonschool.com>
2020-05-04 16:00:52 +0100

    holberto

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,22 +2,21 @@
 """python script"""
 import requests as req
 from sys import argv
-q = req.get("{}users/{}".format(
-    "https://jsonplaceholder.typicode.com/", argv[1]))
-query = req.get("{}todos".format(
-    "https://jsonplaceholder.typicode.com/"), params={"userId": argv[1]})
-q = q.json()
-query = query.json()
-n = q.get("name")
-count = len(query)
-done = 0
-header = []
-for dt in query:
-    if dt.get("completed") is True:
-        done += 1
-        header.append(dt["title"])
-print("Employee {} is done with tasks({}/{}):".format(n, done, count))
-for title in header:
-    print("\t {}".format(title))
 if __name__ == "__main__":
-    pass
+    q = req.get("{}users/{}".format(
+        "https://jsonplaceholder.typicode.com/", argv[1]))
+    query = req.get("{}todos".format(
+        "https://jsonplaceholder.typicode.com/"), params={"userId": argv[1]})
+    q = q.json()
+    query = query.json()
+    n = q.get("name")
+    count = len(query)
+    done = 0
+    header = []
+    for dt in query:
+        if dt.get("completed") is True:
+            done += 1
+            header.append(dt["title"])
+    print("Employee {} is done with tasks({}/{}):".format(n, done, count))
+    for title in header:
+        print("\t {}".format(title))
