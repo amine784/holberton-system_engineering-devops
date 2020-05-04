64d2f0fdb9eb6f0491eff0ca00c5e5f4938c355b
amine784 <1066@holbertonschool.com>
2020-05-04 16:59:25 +0100

    new task mand

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""python script"""
+import csv
+from sys import argv
+import json
+import requests as req
+if __name__ == "__main__":
+    q = req.get("{}users/{}".format(
+        "https://jsonplaceholder.typicode.com/", argv[1]))
+    query = req.get("{}todos".format(
+        "https://jsonplaceholder.typicode.com/"), params={"userId": argv[1]})
+    q = q.json()
+    query = query.json()
+    n = q.get("username")
+    count = len(query)
+    done = 0
+    header = []
+    d = []
+    with open("{}.json".format(argv[1]), "w", newline="") as json_f:
+        dic = {}
+        for dt in query:
+            dic['username'] = q.get("username")
+            dic['complited'] = dt.get('completed')
+            dic['task'] = dt.get('title')
+            d.append(dic)
+        json.dump({argv[1]: dic}, json_f)
