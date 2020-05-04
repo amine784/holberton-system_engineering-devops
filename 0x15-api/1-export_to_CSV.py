6a4bf8552dcf03685204ea18c54879fa5c517de5
amine784 <1066@holbertonschool.com>
2020-05-04 16:06:39 +0100

    new push

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-import csv
 from sys import argv
+import csv
 import requests as req
-q = req.get("{}users/{}".format(
-    "https://jsonplaceholder.typicode.com/", argv[1]))
-query = req.get("{}todos".format(
-    "https://jsonplaceholder.typicode.com/"), params={"userId": argv[1]})
-q = q.json()
-query = query.json()
-n = q.get("name")
-count = len(query)
-done = 0
-header = []
-with open(f"{argv[1]}.csv", "w", newline="") as csv_f:
-    writer = csv.writer(csv_f, quoting=csv.QUOTE_ALL)
-    for dt in query:
-        writer.writerow([argv[1], n, dt.get("completed"),
-                         dt.get('title')])
 if __name__ == "__main__":
-    pass
+    q = req.get("{}users/{}".format(
+        "https://jsonplaceholder.typicode.com/", argv[1]))
+    query = req.get("{}todos".format(
+        "https://jsonplaceholder.typicode.com/"), params={"userId": argv[1]})
+    q = q.json()
+    query = query.json()
+    n = q.get("name")
+    count = len(query)
+    done = 0
+    header = []
+    with open("{}.csv".format(argv[1]), "w", newline="") as csv_f:
+        data = csv.writer(csv_f, quoting=csv.QUOTE_ALL)
+        for dt in query:
+            data.writerow([argv[1], n, dt.get("completed"), dt.get('title')])
