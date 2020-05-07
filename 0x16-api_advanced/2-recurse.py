70151319c7c278f4792486410db41f4ee1d7a943
amine784 <1066@holbertonschool.com>
2020-05-07 13:45:39 +0100

    holberto

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,17 +3,17 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], next=""):
+def recurse(subreddit, hot_list=[], after=None):
     '''function to check nbre of sub'''
     requestpost = requests.get("https://www.reddit.com/r/{}/hot.json".format(
-        subreddit), headers={"User-Agent": "amine", "next": next})
+        subreddit), headers={"User-Agent": "amine"}, params={"after": after})
     if requestpost.status_code != 200:
         return(None)
     response_data = requestpost.json()
     for c in response_data["data"]["children"]:
         hot_list.append(c["data"]["title"])
-    next = response_data["data"]["after"]
-    if next:
+    st = response_data["data"]["after"]
+    if st:
         return(hot_list)
     else:
-        return(recurse(subreddit, hot_list, next))
+        return(recurse(subreddit, hot_list, after))
